a75e30b4b882d1b473200494578bf850f2871177
Taylor Alfers <alfalfatechnologies@gmail.com>
2019-03-16 21:48:54 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+var port = process.env.PORT;
+const app = express();
+const bodyParser = require("body-parser");
+const axios = require('axios');
+var nasaIds = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('home');
+    
+});
+
+app.post('/search', function(req, res){
+    var query = req.body.search;
+    axios.get('https://images-api.nasa.gov/search?q=' + query)
+    .then(function(response){
+        var data = response.data.collection.items;
+        res.render('results', {results: data, query: query, id: nasaIds});
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+});
+
+app.get('/search/:id', function(req, res){
+    axios.get('https://images-api.nasa.gov/search?q=' + req.params.id)
+    .then(function(response){
+        var data = response.data.collection.items;
+        axios.get('https://images-api.nasa.gov/asset/' + req.params.id)
+        .then(function(imgData){
+            var imgUrl = imgData.data.collection.items[0];
+            res.render('show', {item: data, url: imgUrl});
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+});
+    
+app.listen(port, function(){
+    console.log('NASA Image server started on ' + port);
+})
